d1f93b51c87edae2a1ca4ad6875ec9e2069380f5
xusihan <383688501@qq.com>
2017-09-28 19:02:41 +0800

    react

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -206,6 +206,7 @@ ReactDOM.render(
   <LoginControl/>,
   document.getElementById('root')
 );*/
+/*
 
 function Mailbox(props) {
     const unreadMans = props.unreadMans;
@@ -228,5 +229,674 @@ ReactDOM.render(
   <Mailbox unreadMans={messages}/>,
     document.getElementById('root')
 );
+*/
+
+/*
+function WarningBanner(props) {
+    if (!props.warn){
+        return null;
+    }
+    return (
+        <div className="warning">
+            Warning!
+        </div>
+    );
+}
+
+class Page extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {showWarning: true};
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+    handleToggleClick(){
+        this.setState (prevState=>({
+            showWarning: !prevState.showWarning
+        }));
+    }
+    render(){
+        return(
+            <div>
+                <WarningBanner warn={this.state.showWarning}/>
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Page/>,
+    document.getElementById('root')
+)
+*/
+
+/*
+function Warning(props) {
+    if (!props.warn){
+        return null;
+    }
+    return(
+        <h1>Warning!</h1>
+    );
+}
+class Page extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {showWarning:true};
+        this.onClickChangeWarn = this.onClickChangeWarn.bind(this);
+    };
+    onClickChangeWarn(){
+        this.setState (prevState=>({
+            showWarning: !prevState.showWarning
+        }));
+
+    };
+    render(){
+        return(
+           <div>
+               <Warning warn={this.state.showWarning} />
+               <button onClick={this.onClickChangeWarn}>
+                   {this.state.showWarning? 'hide':'show'}
+               </button>
+           </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Page/>,
+    document.getElementById('root')
+);
+*/
+
+/*
+const numbers = [1,2,3,4,5];
+
+const listItems = numbers.map((number)=>
+    <li>{number*2}</li>
+);
+
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('root')
+);
+*/
+
+/*function NumberList(props){
+    const numbers = props.numbers;
+    const listItems = numbers.map((number)=>
+        <li key={number.toString()}>
+            {number}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+const numbers = [1,2,3,4,5,6];
+
+ReactDOM.render(
+   <NumberList numbers={numbers}/>,
+   document.getElementById('root')
+);*/
+
+/*
+const numbers = [1,2,3,4,5];
+
+const listItem = numbers.map((todo,index)=>
+    <li key={index}>
+        {todo}
+    </li>
+);
+
+ReactDOM.render(
+    listItem,
+    document.getElementById('root')
+);
+*/
+/*
+
+const numbers = [1,2,3,4,5];
+
+function Numberlists (props){
+    const listItem = props.numbers.map((todo,index)=>
+        <li key={index}>
+            {todo}
+        </li>
+    );
+    return (
+        <ul>
+            {listItem}
+        </ul>
+    )
+}
+
+ReactDOM.render(
+    <Numberlists numbers={numbers}/>,
+    document.getElementById('root')
+);
+*/
+/*
+
+function ListItem(props){
+    return (
+      <p>{props.listItem}</p>
+    );
+}
+
+function ListItems(props){
+
+    const numbers = props.numbers;
+
+    return (
+        numbers.map((todo,index)=>
+            <li key={index}>
+                <ListItem listItem={todo}/>
+            </li>
+        )
+    )
+}
+
+const numbers = [1,2,3,4,5,6,7];
+
+ReactDOM.render(
+    <ListItems numbers={numbers}/>,
+    document.getElementById('root')
+);
+*/
+
+/*class NameForm extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({value: event.target.value.toUpperCase()});
+    }
+    handleSubmit(event){
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="submit"/>
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(
+  <NameForm/>,
+  document.getElementById('root')
+);*/
+
+/*class TextAreaForm extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {value:''};
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+    handleOnChange(event) {
+        this.setState({value: event.target.value.toUpperCase()});
+    }
+    handleOnSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+    render() {
+        return (
+           <form onSubmit={this.handleOnSubmit}>
+                <label>
+                    name:
+                    <textarea value={this.state.value} onChange={this.handleOnChange}></textarea>
+                </label>
+                <input type="submit" value="submit"/>
+           </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <TextAreaForm/>,
+    document.getElementById('root')
+);*/
+
+/*
+class SelectForm extends React.Component {
+    constructor (props){
+        super(props);
+
+        this.state = {value:'yes'};
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    }
+    handleOnChange(event) {
+        this.setState({value: event.target.value})
+    }
+    handleOnSubmit(event) {
+        alert('your selected name is : ' + this.state.value);
+        event.preventDefault();
+    }
+    render() {
+        return (
+           <form onSubmit={this.handleOnSubmit}>
+               <label>your name:
+                    <select onChange={this.handleOnChange}>
+                        <option>raindrops</option>
+                        <option>yes</option>
+                        <option>rotos</option>
+                    </select>
+               </label>
+               <input type="submit" value='submit'/>
+           </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <SelectForm/>,
+    document.getElementById('root')
+);
+*/
+
+/*class Reservation extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            igGoing: true,
+            numberOfGuests: 2
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox'? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+           [name]: value
+        });
+    }
+    render(){
+        return (
+            <form>
+                <label>
+                    Is going:
+                    <input
+                        name="igGoing"
+                        type="checkbox"
+                        checked={this.state.igGoing}
+                        onChange={this.handleInputChange}
+                    />
+                    <br/>
+                    <label>
+                        number of Guests:
+                        <input
+                            name="numberOfGuests"
+                            type="number"
+                            value={this.state.numberOfGuests}
+                            onChange={this.handleInputChange}
+                        />
+                    </label>
+                </label>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Reservation/>,
+    document.getElementById('root')
+);*/
+/*
+
+function BoilingVerdict(props){
+    if (props.celsius >= 100){
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not bild.</p>
+}
+
+class Calculator extends React.Component {
+    constructor (props){
+        super (props);
+        this.state = {temperature:''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event){
+        this.setState({
+            temperature: event.target.value
+        });
+    }
+    render(){
+        return (
+            <fieldset>
+                <legend>Enter temperature in Ceisius: </legend>
+                <input
+                    value={this.state.temperature}
+                    onChange={this.handleChange}
+                />
+                <BoilingVerdict
+                    celsius={parseFloat(this.state.temperature)}/>
+            </fieldset>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root')
+);
+*/
+/*
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+//Fahrenheit To Celsius
+function toCelsius(fahrenheit){
+    return (fahrenheit - 32) * 5 / 9;
+}
+//Celsius To Fahrenheit
+function toFahrenheit(celsius){
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert){
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)){
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+function BoilingVerdict(props){
+    if (props.celsius >= 100){
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
+class TemperatureInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        this.props.onTemperatureChange(e.target.value);
+    }
+    render(){
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]};</legend>
+                <input value={temperature}
+                    onChange={this.handleChange}/>
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor (props){
+        super (props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+    handleFahrenheitChange(temperature){
+        this.setState({scale: 'f', temperature});
+    }
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div>
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange}
+                />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange}
+                />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Calculator/>,
+    document.getElementById('root')
+);
+*/
+/*
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
+
+function toCelsius(fahrenheit){
+    return (fahrenheit -32 ) * 5/9;
+}
+
+function toFahrenheit(celsius){
+    return (celsius*9 / 5 )+32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)){
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+    handleOnChange(e){
+        this.props.tempertureOnChange(e.target.value);
+    }
+    render(){
+        const scale = this.props.scale;
+        const temperature = this.props.temperature;
+        const temperatureOnChange = this.props.tempertureOnChange;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleOnChange}
+                />
+            </fieldset>
+        );
+    }
+}
+
+class Calculate extends React.Component {
+    constructor (props){
+        super(props);
+        this.state={
+            temperature: '',
+            scale: 'c'
+        };
+        this.fahrenheitOnChange = this.fahrenheitOnChange.bind(this);
+        this.celsiusOnChange = this.celsiusOnChange.bind(this);
+    }
+    fahrenheitOnChange(temperature){
+        this.setState({
+            scale: 'f',
+            temperature
+        });
+    }
+    celsiusOnChange(temperature){
+        this.setState({
+            scale:'c',
+            temperature
+        });
+    }
+    render(){
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+        return (
+            <div>
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    tempertureOnChange={this.celsiusOnChange}
+                />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    tempertureOnChange={this.fahrenheitOnChange}
+                />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)}/>
+            </div>
+        )
+    }
+}
+
+function BoilingVerdict(props){
+    if (props.celsius >= 100){
+        return <p>The water would boil.</p>
+    }
+    return <p>The water would not boil.</p>
+}
+
+ReactDOM.render(
+    <Calculate/>,
+    document.getElementById('root')
+);
+*/
+/*
+
+function FancyBorder(props){
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+}
+
+function Dialog(props){
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+        </FancyBorder>
+    );
+}
+
+function WelcomeDialog(){
+    return (
+        <Dialog
+            title="Welcome"
+            message="Thank you for visiting our spacecraft!"
+        />
+    );
+}
+
+ReactDOM.render(
+    <WelcomeDialog/>,
+    document.getElementById('root')
+);
+*/
+/*
+function FancyBorder(props){
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+}
+function Dialog(props){
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
+            {props.children}
+        </FancyBorder>
+    );
+}
+
+class SignUpDialog extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.state = {login: ''};
+    }
+    render() {
+        return (
+            <Dialog title="Mars Exploration Program"
+                message="How should we refer to you?">
+                <input value={this.state.login}
+                       onChange={this.handleChange} />
+                <button onClick={this.handleSignUp}>
+                    Sign Me Up!
+                </button>
+            </Dialog>
+        );
+    }
+    handleChange(e){
+        this.setState({login: e.target.value});
+    }
+    handleSignUp(){
+        alert(`Welcome aboard, ${this.state.login}!`);
+    }
+}
+
+ReactDOM.render(
+    <SignUpDialog />,
+    document.getElementById('root')
+);
+*/
+
+
+
 
 registerServiceWorker();
